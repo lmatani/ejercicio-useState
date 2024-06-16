27cb4ceeaa08c78462e67b718c550070428f01c2
lmatani <lmatarrubia@gmail.com>
2024-06-16 19:12:55 +0200

    cambios para hacer cambio nombre

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import './App.css'
+import { useState } from 'react';
 
 function App() {
  //Aquí tu código
+ const [name, setName] = useState('Sofía');
+ const [newName, setNewName] = useState('');
+
+ const teachers = ['Data', 'Reyes', 'Yolanda'];
+
+ const listItems = teachers.map(teacher => <li onClick={() => setName(teacher)}>{teacher}</li>);
+
+ return (
+    <div>
+      <h2>Teacher Name: {name}</h2>
+      <ul>{listItems}</ul>
+    </div>
+  );
+
 }
 
 export default App
